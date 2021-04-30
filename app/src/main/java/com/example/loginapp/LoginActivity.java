08946e600f33e56708143b319018af40d5a65d8b
dhiliban <dhiliban.baskar@gmail.com>
2021-04-30 11:51:07 +0530

    Added:Google Sign in

--- app/src/main/java/com/example/loginapp/LoginActivity.java
+++ app/src/main/java/com/example/loginapp/LoginActivity.java
@@ -1,140 +1,192 @@
 package com.example.loginapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
-    EditText emailId, password;
-    Button signIn;
+    private EditText emailId, password;
+    private Button signIn;
+    private SignInButton googleSignIn;
     TextView signUp, forgotPassword;
+    private final String LogInActivity = "LoginActivity";
+    private final int RC_SIGN_IN = 1;
     FirebaseAuth firebaseAuthenticator;
-    private FirebaseAuth.AuthStateListener fireBaseAuthStateListener;
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String isLoggedIn = "Logged_In";
+    GoogleSignInClient mGoogleSignInClient;
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser user = firebaseAuthenticator.getCurrentUser();
+        if(user!=null){
+            Intent intent = new Intent(getApplicationContext(),HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        findViews();
+        setUpOnClickListeners();
+        setForgotPassword();
+    }
+
+    private void findViews() {
         firebaseAuthenticator = FirebaseAuth.getInstance();
         emailId = findViewById(R.id.editTextTextEmailAddress);
         password = findViewById(R.id.editTextTextPassword);
         signIn = findViewById(R.id.button);
         signUp = findViewById(R.id.textView);
         forgotPassword = findViewById(R.id.textView4);
+        googleSignIn = findViewById(R.id.googleSignInButton);
+    }
 
-        fireBaseAuthStateListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser logInFireBaseUser = firebaseAuthenticator.getCurrentUser();
-                if(logInFireBaseUser != null) {
-                    Toast.makeText(LoginActivity.this, "You are logged in", Toast.LENGTH_SHORT).show();
-                    Intent a = new Intent(LoginActivity.this, HomeActivity.class);
-                    startActivity(a);
-                } else {
-                    Toast.makeText(LoginActivity.this, "Please Login", Toast.LENGTH_SHORT).show();
-                }
-            }
-        };
+    private void setUpOnClickListeners() {
+        signIn.setOnClickListener(v -> isLoggingIn());
 
-        signIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = emailId.getText().toString();
-                String pasword = password.getText().toString();
-                if (email.isEmpty()) {
-                    emailId.setError("Please enter a email id");
-                    emailId.requestFocus();
-                } else if (pasword.isEmpty()) {
-                    password.setError("Please enter your password");
-                    password.requestFocus();
-                } else if (email.isEmpty() && pasword.isEmpty()) {
-                        Toast.makeText(LoginActivity.this, "All fields are empty", Toast.LENGTH_SHORT).show();
-                    } else if (!(email.isEmpty() && pasword.isEmpty())) {
-                        firebaseAuthenticator.signInWithEmailAndPassword(email, pasword).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if(!task.isSuccessful()) {
-                                    Toast.makeText(LoginActivity.this, "Login error, try again", Toast.LENGTH_SHORT).show();
-                                } else {
-                                    Intent toHome = new Intent(LoginActivity.this, HomeActivity.class);
-                                    startActivity(toHome);
-                                }
-                            }
-                        });
-                    } else {
-                        Toast.makeText(LoginActivity.this, "Error occurred", Toast.LENGTH_SHORT).show();
-                    }
-            }
+        signUp.setOnClickListener(v -> {
+            Intent intSignUp = new Intent(LoginActivity.this, RegisterActivity.class);
+            startActivity(intSignUp);
+            finish();
         });
 
-        signUp.setOnClickListener(new View.OnClickListener() {
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
+        googleSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent toSignUp = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(toSignUp);
+                signInGoogle();
             }
         });
+    }
+
+    private void signInGoogle() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        firebaseAuthenticator.addAuthStateListener(fireBaseAuthStateListener);
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-        forgotPassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText resetPassword = new EditText(v.getContext());
-                AlertDialog.Builder passwordReset = new AlertDialog.Builder(v.getContext());
-                passwordReset.setTitle("Reset Password");
-                passwordReset.setTitle("Enter your email address to reset your password");
-                passwordReset.setView(resetPassword);
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                assert account != null;
+                Log.d(LogInActivity, "firebaseAuthWithGoogle:" + account.getId());
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(LogInActivity, "Google sign in failed", e);
+            }
+        }
+    }
 
-                passwordReset.setPositiveButton("Y", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        String mail = resetPassword.getText().toString();
-                        firebaseAuthenticator.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void aVoid) {
-                                Toast.makeText(LoginActivity.this, "Reset link sent to your given email", Toast.LENGTH_SHORT).show();
-                            }
-                        }).addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Toast.makeText(LoginActivity.this, "Error sending link" + e.getMessage(), Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    }
-                });
-
-                passwordReset.setNegativeButton("N", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                    }
-                });
-
-                passwordReset.create().show();
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        firebaseAuthenticator.signInWithCredential(credential).addOnCompleteListener(LoginActivity.this, task -> {
+            if (task.isSuccessful()) {
+                // Sign in success,
+                Log.d(LogInActivity, "signInWithCredential:success");
+                firebaseAuthenticator.getCurrentUser();
+                Intent toHomePage = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(toHomePage);
+                finish();
+            } else {
+                // If sign in fails, display a message to the user.
+                Log.w(LogInActivity, "signInWithCredential:failure", task.getException());
             }
         });
     }
-}
+
+    private void setForgotPassword() {
+            forgotPassword.setOnClickListener(v -> {
+                final EditText resetMail = new EditText(v.getContext());
+                final AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+                passwordResetDialog.setTitle("Reset Password ?");
+                passwordResetDialog.setMessage("Enter Your Email Address");
+                passwordResetDialog.setView(resetMail);
+
+                passwordResetDialog.setPositiveButton("Reset", (dialog, which) -> {
+                    String mail = resetMail.getText().toString();
+                    firebaseAuthenticator.sendPasswordResetEmail(mail).addOnSuccessListener(aVoid -> Toast.makeText(LoginActivity.this, "Reset Link Sent To Your Email.", Toast.LENGTH_SHORT).show()).addOnFailureListener(e -> Toast.makeText(LoginActivity.this, "Please Check your Network Connection Or email Address", Toast.LENGTH_SHORT).show());
+                });
+
+                passwordResetDialog.setNegativeButton("Cancel", (dialog, which) -> {
+                    // back to login page
+                });
+
+                passwordResetDialog.create().show();
+            });
+        }
+
+        public void isLoggingIn() {
+            String email = emailId.getText().toString();
+            String pasword = password.getText().toString();
+            if (email.isEmpty()) {
+                emailId.setError("Please enter email id");
+                emailId.requestFocus();
+            } else if (pasword.isEmpty()) {
+                password.setError("Please enter your password");
+                password.requestFocus();
+            } else if (!(email.isEmpty() && pasword.isEmpty())) {
+                firebaseAuthenticator.signInWithEmailAndPassword(email, pasword).addOnCompleteListener(LoginActivity.this, task -> {
+                    if (!task.isSuccessful()) {
+                        Toast.makeText(LoginActivity.this, "Login Error, Please try again", Toast.LENGTH_SHORT).show();
+                    } else {
+                        SharedPreferences mySharedPref = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+                        SharedPreferences.Editor editor = mySharedPref.edit();
+                        editor.putBoolean(isLoggedIn, true);
+                        editor.apply();
+                        finish();
+                        Intent toHome = new Intent(LoginActivity.this, HomeActivity.class);
+                        startActivity(toHome);
+                    }
+                });
+            } else {
+                Toast.makeText(LoginActivity.this, "Error Occurred While logging in", Toast.LENGTH_SHORT).show();
+
+            }
+        }
+    }
+
+
 
