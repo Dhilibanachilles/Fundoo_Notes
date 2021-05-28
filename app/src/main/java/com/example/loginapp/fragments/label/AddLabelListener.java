4b2cd6a5c764d49d1dd40701a9a4fcf9ab9b1b59
dhiliban <dhiliban.baskar@gmail.com>
2021-05-28 15:43:29 +0530

    Added:Label listener

---
+++ app/src/main/java/com/example/loginapp/fragments/label/AddLabelListener.java
@@ -0,0 +1,7 @@
+package com.example.loginapp.fragments.label;
+
+import com.example.loginapp.data_manager.model.FirebaseLabelModel;
+
+public interface AddLabelListener {
+    void onLabelAdded(FirebaseLabelModel label);
+}
